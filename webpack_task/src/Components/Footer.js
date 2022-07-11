e2193adcaedb21a0fe85d46bdbe4e0fa3b336434
vishakha_agrawal <vishakha_agrawal@epam.com>
2022-07-11 14:31:29 +0530

    webpack task

---
+++ webpack_task/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer=()=>{
+    return(
+        <>
+        <div className="footer">
+        @NewsFeed2019
+        </div>
+        </>
+    )
+}
+export default Footer;
---
+++ webpack_task/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Header=()=>{
+    return(
+        <>
+        <div className="header">
+        <div>
+                <h3>NEWS BLOG</h3>
+                <h5>WORLDWIDE NEWS</h5>
+              </div>
+        </div>
+        </>
+    )
+}
+export default Header;
